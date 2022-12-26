cd2184f52c908dd9acaeb99af2b978bfab2bf2d9
cagan ozen <117105190+caganozn@users.noreply.github.com>
2022-12-26 23:50:49 +0300

    Update AdaPark.java

--- AdaPark.java
+++ AdaPark.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class adapark {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Kac adet Ada Sarp varyasyonu istersiniz?");
@@ -9,29 +10,37 @@ public class adapark {
         String[] adamsarp = new String[length];
 
         for (int i = 0; i < length; i++) {
+
             System.out.println((i + 1) + ". varyasyonu giriniz");
             String newpak = sc.next();
+
             String pak = newpak.substring(0, 1).toLowerCase() + newpak.substring(1);
             String sarp = newpak.substring(0, 1).toUpperCase() + newpak.substring(1);
+
             String var1 = ("Ada " + sarp + " Öz" + pak);
             adamsarp[i] = var1;
         }
         for (int i = 0; i < length; i++) {
             System.out.println(adamsarp[i]);
         }
+
         System.out.println("Bu isimlerin hangisini kullanmak istersiniz? [1,2,3 olarak seçiniz]");
 
         int temp = sc.nextInt();
         String karakter = adamsarp[temp - 1];
+
         System.out.println("Sectiginiz karakter " + karakter + ". Onayliyor musunuz? [evet / hayir]");
         String ans = sc.next();
+
         if (ans.equals("evet")) {
             System.out.println("Yeni karakteriniz " + karakter + ".");
         }
+
         if (ans.equals("hayir")) {
-            System.out.println("Program tekrardan baslatiliyor");
-        } else
+            System.out.println("Baska bir isim yaratmaniz icin program tekrardan baslatiliyor.");
+        } else {
             System.out.println("Tekrardan yapmaniz icin sistem tekrardan calistirilacaktir.");
-        System.exit(0);
+            System.exit(0);
+        }
     }
 }
